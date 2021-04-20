d88aa596a59890c67ea3ed9163472f571932819e
DarkNinja3141 <DarkNinja3141@gmail.com>
2021-04-19 22:59:25 -0400

    Add command to request post link
    Version 0.2.0

--- component/RedditSlashCommands.py
+++ component/RedditSlashCommands.py
@@ -1,11 +1,11 @@
-from asyncpraw.exceptions import InvalidURL
 from asyncpraw.reddit import Submission
 from discord_slash import cog_ext, SlashContext, SlashCommandOptionType
 from discord_slash.utils import manage_commands
+from discord_slash.utils.manage_commands import create_choice
 
 from util import debug_guilds
 from .MyCog import MyCog
-from command.reddit import get_reddit_embed
+from command.reddit import get_reddit_embed, SubmissionType
 
 
 class RedditSlashCommands(MyCog):
@@ -21,13 +21,29 @@ class RedditSlashCommands(MyCog):
                                option_type=SlashCommandOptionType.STRING,
                                required=True,
                            ),
+                           manage_commands.create_option(
+                               name="request_info",
+                               description="Privately obtain post info",
+                               option_type=SlashCommandOptionType.STRING,
+                               required=False,
+                               choices=[
+                                   create_choice(
+                                       name="Link",
+                                       value="link",
+                                   ),
+                                   # create_choice(
+                                   #     name="Gallery",
+                                   #     value="gallery",
+                                   # ),
+                               ],
+                           ),
                        ],
                        guild_ids=debug_guilds(),
                        )
-    async def reddit(self, ctx: SlashContext, url: str):
+    async def reddit(self, ctx: SlashContext, url: str, request_info: str = None):
         try:
             submission: Submission = await self.bot.reddit.submission(url=url)
-        except InvalidURL:
+        except:
             await ctx.send(content="Invalid URL", hidden=True)
             return
         if submission.over_18:
@@ -35,5 +51,17 @@ class RedditSlashCommands(MyCog):
                 await ctx.send(content="NSFW submissions must be in an NSFW channel", hidden=True)
                 return
 
-        content, embed = await get_reddit_embed(self.bot.reddit, submission)
-        await ctx.send(content=content, embed=embed)
+        if request_info is None:
+            content, embed = await get_reddit_embed(self.bot.reddit, submission)
+            hidden = False
+        elif request_info == "link":
+            if SubmissionType.get_submission_type(submission).is_self():
+                await ctx.send(content="Post must be a link post", hidden=True)
+                return
+            content = submission.url
+            embed = None
+            hidden = True
+        else:
+            await ctx.send(content="Invalid type", hidden=True)
+            return
+        await ctx.send(content=content, embed=embed, hidden=hidden)
